956778a2c16f7f6a6c00f0912ba18c7a0e060da3
LoganMinarik <168377908+LoganMinarik@users.noreply.github.com>
2024-12-29 19:55:33 -0500

    Create pythoncircle.py

---
+++ pythoncircle.py
@@ -0,0 +1,10 @@
+radius = 2
+area_of_circle = 3.14159 * radius * radius
+
+print('For a circle of radius')
+print(radius)
+print('the area is:')
+print(area_of_circle)
+
+print('The type of the variable radius is', type(radius))
+print('The type of area_of_circle variable is', type(area_of_circle))
